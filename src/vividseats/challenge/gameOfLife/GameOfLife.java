69c9a474cfe54631978916365c9e44f6d73129ba
EugenioMoreno <eugenio.morenog@gmail.com>
2016-07-19 21:13:42 -0500

    Comments added

--- src/vividseats/challenge/gameOfLife/GameOfLife.java
+++ src/vividseats/challenge/gameOfLife/GameOfLife.java
@@ -10,7 +10,7 @@ public class GameOfLife {
         Board board=new Board(3,3);
         //Initialize it with random values
         board.setRandomGrid();
-        
+
         //Print the first generation
         System.out.println("Generation"+generation);
         board.printBoard();
@@ -24,19 +24,21 @@ public class GameOfLife {
         tiedGame=true;
         endGame=false;
         generation++;
+        //Create and set up a buffer for the Board
         Board auxBoard=new Board(b.getyGrid(),b.getxGrid());
         auxBoard.setRandomGrid();
-        //Copy the board values
         auxBoard.copyBoardFrom(b);
+        //Evaluate each cell's neighbors and apply a rule
         for (int i = 0; i <b.getyGrid() ; i++) {
             for (int j = 0; j < b.getxGrid(); j++) {
-                //Current cell that we are evaluating
+                //Current cell that it is being evaluated
                 Cell currentCell=b.getBoardGrid()[i][j];
                 //index 0 alive; index 1 dead
                 int[] neighbors=countNeighbors(b.getBoardGrid(),i,j);
                 int aliveNeighbors = neighbors[0];
-
+                //Classify the cell and save the future status in to the buffer
                 auxBoard.getBoardGrid()[i][j].setAlive(classifyCell(currentCell,aliveNeighbors));
+                //Look for different value between iterations
                 if (auxBoard.getBoardGrid()[i][j].isAlive()== !b.getBoardGrid()[i][j].isAlive()){
                     tiedGame=false;
                 }
@@ -45,6 +47,7 @@ public class GameOfLife {
         b.copyBoardFrom(auxBoard);
         System.out.println("Generation"+generation);
         b.printBoard();
+        //Check if there is a final situation
         checkResult(b);
     }
 
@@ -68,11 +71,12 @@ public class GameOfLife {
 
     public static int[] countNeighbors(Cell[][] b, int y, int x){
         int[] neighbors={0,0};
+        //Evaluate neighbors
         for (int i = y-1; i <=y+1 ; i++) {
             for (int j = x-1; j <=x+1 ; j++) {
 
                 if(i==y&&j==x){
-                    //This does not count
+                    //This does not count because it is the cell itself
                 }else if(i<0||j<0||i>=b.length||j>=b[0].length){
                 //If we are out of the grid we will look at the next neighbor
                 continue;
